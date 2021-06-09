54cbcade287a8da2d5d6a0f6fff80b8532c69278
Hadi Abaza <Hadiabaza1@gmail.com>
2021-06-09 19:02:43 -0400

    commit tree 4e0049a8dbcb930c57a139e27eb2c1c54065fe58

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+function saturdayFun(event = "roller-skate") {
+  return `This Saturday, I want to ${event}` + "!";
+}
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}` + ".";
+}
+
+function wrapAdjective() {}
